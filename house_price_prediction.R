a534556a8752fab10d747375f83d553ae3ee20c9
Murugesan <mrk.ram108@gmail.com>
2018-09-16 15:42:23 -0400

    Updated version - knn

--- house_price_prediction.R
+++ house_price_prediction.R
@@ -16,9 +16,9 @@ test_input <- read.csv("test.csv")
 train_input$input <- "Train"
 test_input$input <- "Test"
 
-dim(train)
+dim(train_input)
 #[1] 1460   81
-dim(test)
+dim(test_input)
 test_input$SalePrice <- NA
 #[1] 1459   80
 
@@ -27,58 +27,28 @@ train_all <- rbind(train_input,test_input)
 ########### DATA ANALYSIS FOR PRE-PROCESSING ####################################
 
 #Finding unique values under each column
-unique_counts_df <- data.frame(unique_values = sapply(train,function(x)length(unique(x))))
+unique_counts_df <- data.frame(unique_values = sapply(train_all,function(x)length(unique(x))))
 unique_counts_df$col_names <- row.names(unique_counts_df)
 categories <- unique_counts_df[unique_counts_df$unique_values <= 2,]
 
 #Finding the number of NAs across each column
-NA_counts_df <- data.frame(NA_values = sapply(train,function(x)sum(is.na(x))))
+NA_counts_df <- data.frame(NA_values = sapply(train_all,function(x)sum(is.na(x))))
 NA_counts_df$columns <- row.names(NA_counts_df)
 
 #Top NA values
 Top_NA_values <- NA_counts_df %>% arrange(desc(NA_values)) %>% 
-                mutate(perc = round(NA_values/nrow(train) * 100,2)) %>% filter(NA_values != 0)
+                mutate(perc = round(NA_values/nrow(train_all) * 100,2)) %>% filter(NA_values != 0)
 #Top 5 features cannot be used even for NA interpolation since we have high number of NA values - above ~50%
 # We can interpolate the values from 'GarageType' to 'Electrical'
 
 
 ############### NA TREATMENT #########################
 
-
-train_all$PoolArea <- NULL
-train_all$MiscFeature <- NULL
-train_all$Alley <- NULL
-train_all$Fence  <- NULL
-train_all$FireplaceQu <- NULL
-
-
-train_all$GarageType[is.na(train_all$GarageType)] <- "Attchd"
-
-
-
-
-train_all[is.na(train_all)] <- 0
-
-
+##Calling the NA treatment function to treat the missing values
+train_all <- na_treatment(train_all,Top_NA_values)
 
 ######################################################################
 
-
-# Building a baseline linear model
-remove_cols <- c(Top_NA_values[1:6,"columns"],"Exterior1st","ExterCond","Foundation","Heating")
-
-lm_formula <- formula(paste0("SalePrice ~ . "))
-
-lm_model <- lm(SalePrice ~ . , data = train[!names(train) %in% remove_cols])
-#summary(lm_model)
-
-test_preds <- predict(lm_model,test)
-
-test_preds[is.na(test_preds)] <- mean(test_preds,na.rm= TRUE)
-
-
-submission <- data.frame(Id = test$Id, SalePrice = test_preds)
-#write.csv(submission,"Test_submission.csv",row.names = FALSE)
 
 ########### EXPLORATORY DATA ANALYSIS #################
 
@@ -86,23 +56,31 @@ submission <- data.frame(Id = test$Id, SalePrice = test_preds)
 ############### FEATURE ENGINEERING ################
 
 
-
-
 ### Selecting the final set of columns for the output 
-remove_cols <- names(train_all)[!sapply(train_all,is.integer)]
-req_cols <- names(train_all)[!names(train_all) %in% c(remove_cols,"Id")]
+# remove_cols <- names(train_all)[!sapply(train_all,is.integer)]
+# req_cols <- names(train_all)[!names(train_all) %in% c(remove_cols,"Id")]
+# 
+
+remove <- c("input","Id")
+
+train <- train_all[train_all$input == "Train",!names(train_all) %in% remove]
 
 
-train_all_scale <- train_all
-train_all_scale[req_cols] <- data.frame(apply(train_all[req_cols],2,scale))
+summary(lm(SalePrice ~ ., data = train))
+
+
+
+model_matrix <- stats::model.matrix(SalePrice + input ~., train_all)
+train_all_scale <- model_matrix
+train_all_scale <- data.frame(apply(model_matrix,2,scale))
 train_all_scale$SalePrice <- train_all$SalePrice
  
 
 ############## TRAINING AND HOLDOUT SET ##############
 
 
-train <- train_all_scale[train_all_scale$input == "Train",]
-test <-  train_all_scale[train_all_scale$input == "Test",]
+train <- train_all_scale[train_all_scale$inputTrain > 0,]
+test <-  train_all_scale[train_all_scale$inputTrain < 0,]
 
 set.seed(0)
 train_records <- sample(1:nrow(train),0.8 * nrow(train))
@@ -113,6 +91,28 @@ holdout_data <- train[holdout_records,]
 
 ######### PARAMETRIC MODEL APPROACH ###############
 
+req_cols_lm <- names(train_data)[!names(train_data) %in% c("inputTrain","X.Intercept.","Id")]
+#Baseline model with linear regression
+lm_model <- glm(SalePrice ~ . , data = train_data[req_cols_lm])
+#summary(lm_model)
+
+#Predictint the test data
+test_preds <- predict(lm_model,holdout_data)
+
+## Checking the residual
+sqrt(sum((as.numeric(test_preds) - holdout_data$SalePrice)^2)/length(test_preds))
+
+
+##### Training and predicting on actual  test
+
+lm_model <- lm(SalePrice ~ . , data = train[req_cols_lm])
+#summary(lm_model)
+
+test_preds <- predict(lm_model,test)
+
+submission <- data.frame(Id = test_input$Id, SalePrice = test_preds)
+write.csv(submission,"Test_submission.csv",row.names = FALSE)
+
 
 ######## NON PARAMETRIC MODEL APPROACH ############
 ### KNN ###
@@ -120,8 +120,9 @@ holdout_data <- train[holdout_records,]
 #Finding the best parameters using holdout set
 
 k <- 25
-req_cols <- req_cols[!req_cols %in% c("SalePrice")]
+req_cols <- names(train_data)[!names(train_data) %in% c("SalePrice","inputTrain","X.Intercept.","Id")]
 holdout_price_all <- c()
+
 
 for(records in 1:nrow(holdout_data)){
   
@@ -135,8 +136,9 @@ for(records in 1:nrow(holdout_data)){
   row.names(df2) <- NULL
   df2 <- as.matrix(df2)
   
+  #Calculating the Euclidiean distance
   distance_each <- sqrt(rowSums((as.vector(df2) - df1)^2))
-  names(distance_each) <- 1:length(distance_each)
+  names(distance_each) <- 1:nrow(train_data)
   
   positions <- head(sort(distance_each),k)
   
@@ -150,8 +152,11 @@ for(records in 1:nrow(holdout_data)){
   
 }
 
-sum((holdout_price_all - holdout_data$SalePrice)^2)/1e10
 
+
+
+holdout_priceall <- FNN::knn.reg(train_data[req_cols], holdout_data[req_cols],train_data$SalePrice, k = 5)
+sqrt(sum((holdout_priceall$pred - holdout_data$SalePrice)^2)/length(holdout_priceall$pred))
 
 
 
@@ -195,7 +200,9 @@ for(records in 1:nrow(test)){
 
 length(test_price_all)
 
-submission <- data.frame(Id = test_input$Id, SalePrice = test_price_all)
+
+test_price_all <- FNN::knn.reg(train[req_cols], test[req_cols],train$SalePrice, k = 3)
+submission <- data.frame(Id = test_input$Id, SalePrice = test_price_all$pred)
 write.csv(submission,"Test_submission.csv",row.names = FALSE)
 
 
